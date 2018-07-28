c2f79fa5ba0bb0e76fd8729d098521d68646025a
hjalmar <hjalmar.basile@icloud.com>
2018-07-28 13:50:42 +0200

    Delete selected pet from database

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -253,7 +253,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
-                // Do nothing for now
+                showDeleteConfirmationDialog();
                 return true;
             // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home: {
@@ -324,6 +324,57 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
         // Create and show the AlertDialog
         builder.show();
+    }
+
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deletePet();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        builder.show();
+    }
+
+    /**
+     * Perform the deletion of the pet in the database.
+     */
+    private void deletePet() {
+        if (mCurrentPetUri == null) {
+            throw new IllegalStateException("Pet deletion was requested, but no petUri was found");
+        }
+
+        try {
+            String msg;
+
+            int rowsDelete = getContentResolver().delete(mCurrentPetUri, null, null);
+            if (rowsDelete > 0) {
+                msg = getString(R.string.editor_delete_pet_successful);
+            } else {
+                msg = getString(R.string.editor_delete_pet_failed);
+            }
+
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finish();
     }
 
     @NonNull
